7882f0af7803a39ae3697d481859d30dd8ba7c45
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-06-30 14:23:32 -0300

    Add files via upload

---
+++ cpf/iterable.cs
@@ -0,0 +1,7 @@
+// using System;
+// string cpf = "96757825872";
+
+// foreach (char c in cpf) {
+//     Console.WriteLine (c);
+//     //elemento.send_keys (c);
+// }
---
+++ cpf/selenium_test.cs
@@ -0,0 +1,36 @@
+using System;
+using NUnit;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+[TestFixture]
+public class TestProgram {
+    private IWebDriver driver;
+    [SetUp]
+    public void SetupTest () {
+        driver = new FirefoxDriver ();
+        driver.Manage ().Timeouts ().ImplicitWait = TimeSpan.FromSeconds (30);
+        driver.Url = "https://igorescobar.github.io/jQuery-Mask-Plugin/";
+    }
+
+    [TearDown]
+    public void TeardownTest () {
+        driver.Quit ();
+    }
+
+    [Test]
+    public void testMethod () {
+        String cpf = "96757825872";
+        String title = driver.Title;
+        System.Console.WriteLine ("title of site is: " + title);
+        IWebElement field_cpf = driver.FindElement (By.Id ("cpf"));
+        foreach (char c in cpf) {
+            field_cpf.SendKeys (c.ToString ());
+        }
+        // Validando o resultado esperado!
+        IWebElement field_cpf_after = driver.FindElement (By.Id ("cpf"));
+        Console.WriteLine (field_cpf_after.Text);
+        Assert.AreEqual ("967.578.258-72", field_cpf_after.GetAttribute ("value"));
+    }
+}
